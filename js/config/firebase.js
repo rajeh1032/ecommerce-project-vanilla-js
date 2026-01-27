07e52efad283800eb00bfbc1159ac28292a83525
rajeh1032 <98366239+rajeh1032@users.noreply.github.com>
2026-01-27 15:32:40 +0200

    Implement authentication functionality with login and registration forms

---
+++ js/config/firebase.js
@@ -0,0 +1,17 @@
+// js/firebase/firebaseService.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCnM5ndqZOyosxKtyrmfMAhIRMuymDDAqM",
+  authDomain: "khordaclick.firebaseapp.com",
+  projectId: "khordaclick",
+  storageBucket: "khordaclick.firebasestorage.app",
+  messagingSenderId: "941077442352",
+  appId: "1:941077442352:web:461389879b9ef02a02a2f5",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
